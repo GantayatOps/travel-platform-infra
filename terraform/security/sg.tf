01293d999acae9b81a2917866e7e709728172801
GantayatOps <shubhamgantayat@gmail.com>
2026-03-24 02:08:15 +0530

    Day 2: Add app security group for private instances
    - Created app_sg for private EC2 instances
    
    - Allowed SSH access only from bastion_sg using SG referencing
    
    - Configured outbound rules for full egress

--- terraform/security/sg.tf
+++ terraform/security/sg.tf
@@ -1,4 +1,4 @@
-# SG for Bastion Host
+# Bastion Host SG
 resource "aws_security_group" "bastion_sg" {
   name   = "bastion-sg"
   vpc_id = var.vpc_id
@@ -8,6 +8,27 @@ resource "aws_security_group" "bastion_sg" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# App SG
+resource "aws_security_group" "app_sg" {
+  name   = "app-sg"
+  vpc_id = var.vpc_id
+
+  # Allow SSH ONLY from bastion_sg
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id]
   }
 
   egress {
