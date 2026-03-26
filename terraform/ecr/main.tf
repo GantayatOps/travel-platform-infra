41705791c7b59d5e3e3eae9960ea0c93822c305e
GantayatOps <shubhamgantayat@gmail.com>
2026-03-27 01:45:06 +0530

    feat(ecr): add lifecycle policy to retain last 5 images

--- terraform/ecr/main.tf
+++ terraform/ecr/main.tf
@@ -1,3 +1,4 @@
+# ECR Repository
 resource "aws_ecr_repository" "app_repo" {
   name                 = "travel-app-repo"
   image_tag_mutability = "MUTABLE"
@@ -6,11 +7,29 @@ resource "aws_ecr_repository" "app_repo" {
     scan_on_push = true
   }
 
-#   encryption_configuration {
-#     encryption_type = "AES256"
-#   }
-
   tags = {
     Name = "travel-app-ecr"
   }
+}
+
+# Keep last 5 images only
+resource "aws_ecr_lifecycle_policy" "keep_last_5" {
+  repository = aws_ecr_repository.app_repo.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last 5 images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 5
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
 }
