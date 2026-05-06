34fe67cc5d81c0b6102c318bebcc26d871a959d7
GantayatOps <shubhamgantayat@gmail.com>
2026-05-07 01:14:35 +0530

    fix(terraform): scope S3 upload notifications

--- terraform/notifications/s3_to_sqs.tf
+++ terraform/notifications/s3_to_sqs.tf
@@ -2,7 +2,8 @@ resource "aws_s3_bucket_notification" "s3_to_sqs" {
   bucket = var.bucket_id
 
   queue {
-    queue_arn = var.sqs_queue_arn
-    events    = ["s3:ObjectCreated:*"]
+    queue_arn     = var.sqs_queue_arn
+    events        = ["s3:ObjectCreated:*"]
+    filter_prefix = "trips/"
   }
 }
